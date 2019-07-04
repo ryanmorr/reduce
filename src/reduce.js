e159665516aa19eca5bb956dfef19b5de834650e
Ryan Morr <rm.morr@gmail.com>
2019-07-04 13:15:24 -0400

    Add support for iterable objects

--- src/reduce.js
+++ src/reduce.js
@@ -1,5 +1,12 @@
-export default function reduce(obj, initialValue, callback) {
+export default function reduce(obj, value, callback) {
     if (Array.isArray(obj)) {
-        return obj.reduce(callback, initialValue);
+        return obj.reduce(callback, value);
+    }
+    if (typeof obj[Symbol.iterator] === 'function') {
+        let i = 0;
+        for (let entry of obj) {
+            value = callback(value, entry, i++, obj);
+        }
+        return value;
     }
 }
--- test/specs/reduce.js
+++ test/specs/reduce.js
@@ -6,9 +6,9 @@ describe('reduce', () => {
         const array = [1, 2, 3];
         let currentIndex = 0;
 
-        const returnValue = reduce(array, 0, (acc, val, i, arr) => {
+        const returnValue = reduce(array, 0, (acc, val, i, obj) => {
             expect(currentIndex).to.equal(i);
-            expect(arr).to.equal(array);
+            expect(obj).to.equal(array);
             expect(array[currentIndex]).to.equal(val);
             if (i === 0) {
                 expect(acc).to.equal(0);
@@ -18,6 +18,34 @@ describe('reduce', () => {
                 expect(val).to.equal(2);
             } else if (i === 2) {
                 expect(acc).to.equal(3);
+                expect(val).to.equal(3);
+            }
+            currentIndex++;
+            return acc + val;
+        });
+
+        expect(returnValue).to.equal(6);
+    });
+
+    it('should reduce an iterable object', () => {
+        const map = new Map([['a', 1], ['b', 2], ['c', 3]]);
+        let currentIndex = 0;
+
+        const returnValue = reduce(map, 0, (acc, [key, val], i, obj) => {
+            expect(currentIndex).to.equal(i);
+            expect(obj).to.equal(map);
+            expect(map.get(key)).to.equal(val);
+            if (i === 0) {
+                expect(acc).to.equal(0);
+                expect(key).to.equal('a');
+                expect(val).to.equal(1);
+            } else if (i === 1) {
+                expect(acc).to.equal(1);
+                expect(key).to.equal('b');
+                expect(val).to.equal(2);
+            } else if (i === 2) {
+                expect(acc).to.equal(3);
+                expect(key).to.equal('c');
                 expect(val).to.equal(3);
             }
             currentIndex++;
